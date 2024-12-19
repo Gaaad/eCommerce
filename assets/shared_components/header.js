27c201f15e43ba1f710fd96ebd271a0b94eb0caf
Mohamed-Nabi1 <mohamedd.nabil3@gmail.com>
2024-12-19 20:52:59 +0200

    modify header

--- assets/shared_components/header.js
+++ assets/shared_components/header.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const headerHTML = `
+    const headerHTML = `
        <header>
     <nav>
         <div class="logo">Exclusive</div>
@@ -11,22 +11,17 @@ document.addEventListener("DOMContentLoaded", () => {
         </ul>
         <div class="search-box">
             <ul class="nav-links">
-<<<<<<< HEAD
-                <li><img src="./assets/images/general/heart.png" alt="" onclick=""></li>
-                <li><img src="./assets/images/general/shopping-cart.png" onclick="window.location.href='./cart.html'"></li>
-=======
                 <li><img src="./assets/images/general/heart.png" alt="" onclick="">
                     <div class="counter">1</div>
                 </li>
                 <li><img src="./assets/images/general/shopping-cart.png">
                     <div class="counter">1</div>
                 </li>
->>>>>>> 7cb94af148d5a36bd577743ef88b6266c42c756a
                 <li><img src="./assets/images/general/user.png" alt=""></li>
             </ul>
         </div>
     </nav>
 </header>
     `;
-  document.getElementById("header").innerHTML = headerHTML;
+    document.getElementById("header").innerHTML = headerHTML;
 });
